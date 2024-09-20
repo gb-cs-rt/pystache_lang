38237953a8e549bff1ffa9946cccd7d6f1d6ecb5
Baggiio <gustavo.gbaggio@gmail.com>
2024-09-20 13:50:20 -0300

    added file argument to compiler

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from compiler import Token, Lexer
 from pprint import pp
+import argparse
 
 def main():
 
-    data = open("src/specifications.spl", "r").read()
+    parser = argparse.ArgumentParser(description='Compiler for the Pystache language')
+    parser.add_argument("file", nargs="?", default=None, type=str)
+    args = parser.parse_args()
+
+    file = args.file if args.file else "src/test.pyst"
+
+    data = open(file, "r").read()
     lexer = Lexer(data)
     tokens = lexer.getTokens()
     pp(tokens)
