054ca4260de3d03efc5f0ae0452c745de8d9ce27
Baggiio <unifgbagio@fei.edu.br>
2024-10-02 19:04:41 +0000

    merged parserTree into parser

--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,13 @@
+from utils import Tree
+
 # ========================================
 # >>>>>>>>>>> Classe Rules <<<<<<<<<<<<<<<
 # ========================================
 
 class Rules:
-
     def __init__(self, parser):
         self.parser = parser
-
+        self.tree = parser.tree
         self.nextToken = parser.nextToken
         self.matchLexema = parser.matchLexema
         self.matchTipo = parser.matchTipo
@@ -14,138 +15,134 @@ class Rules:
         self.eof = parser.eof
         self.error = parser.error
         self.endBlock = parser.endBlock
+        self.addNode = parser.addNode
+        self.setRoot = parser.setRoot
 
     # ============== REGRAS ==================
-        
+
     def prog(self):
         # prog -> bloco
-        print("-> prog")
+        node = self.setRoot()
+        return True if self.bloco(node) and self.eof() else self.error(node)
 
-        return True if self.bloco() and self.eof() else self.error("prog")
-    
-    def bloco(self):
+    def bloco(self, parent_node):
         # bloco -> cmd bloco | ε
-        print("-> bloco")
+        node = self.addNode("bloco", parent_node)
+        if self.cmd(node):
+            return True if self.eof() or self.endBlock() or self.bloco(node) else self.error(node)
 
-        if self.cmd():
-            return True if self.eof() or self.endBlock() or self.bloco() else self.error("bloco")
-    
-    def cmd(self):
+    def cmd(self, parent_node):
         # cmd -> cmdIf
-        print("-> cmd")
-
+        node = self.addNode("cmd", parent_node)
         if self.firstFollow("RESERVED_SE"):
-            return self.cmdIf()
+            return self.cmdIf(node)
         if self.firstFollow("RESERVED_PASSE"):
-            return self.matchTipo("RESERVED_PASSE")
-        
-        return self.error("cmd")
-    
-    def cmdIf(self):
+            return self.matchTipo("RESERVED_PASSE", node)
+        return self.error(node)
+
+    def cmdIf(self, parent_node):
         # cmdIf -> RESERVED_SE condicao RESERVED_ENTAO INDENT bloco DEDENT cmdElse
-        print("-> cmdIf")
+        node = self.addNode("cmdIf", parent_node)
+        return True if self.matchTipo("RESERVED_SE", node) and self.condicao(node) and self.matchTipo("RESERVED_ENTAO", node) and self.matchTipo("INDENT", node) and self.bloco(node) and self.matchTipo("DEDENT", node) and self.cmdElse(node) else self.error(node)
 
-        return True if self.matchTipo("RESERVED_SE") and self.condicao() and self.matchTipo("RESERVED_ENTAO") and self.matchTipo("INDENT") and self.bloco() and self.matchTipo("DEDENT") and self.cmdElse() else self.error("cmdIf")
-    
-    def cmdElse(self):
+    def cmdElse(self, parent_node):
         # cmdElse -> RESERVED_SENAO INDENT bloco DEDENT | ε
-        print("-> cmdElse")
-
-        if self.matchTipo("RESERVED_SENAO"):
-            return True if self.matchTipo("INDENT") and self.bloco() and self.matchTipo("DEDENT") else self.error("cmdElse")
+        node = self.addNode("cmdElse", parent_node)
+        if self.matchTipo("RESERVED_SENAO", node):
+            return True if self.matchTipo("INDENT", node) and self.bloco(node) and self.matchTipo("DEDENT", node) else self.error(node)
         return True
-    
-    def condicao(self):
+
+    def condicao(self, parent_node):
         # condicao -> relacao opLogico relacao | relacao
-        print("-> condicao")
-        if self.relacao():
+        node = self.addNode("condicao", parent_node)
+        if self.relacao(node):
             if self.firstFollow("AND") or self.firstFollow("OR"):
-                return True if self.opLogico() and self.relacao() else self.error("condicao")
+                return True if self.opLogico(node) and self.relacao(node) else self.error(node)
             return True
-        
-    def relacao(self):
+
+    def relacao(self, parent_node):
         # relacao -> valor opRelacional valor | valor
-        print("-> relacao")
-        if self.valor():
+        node = self.addNode("relacao", parent_node)
+        if self.valor(node):
             if self.firstFollow("GREATER") or self.firstFollow("LESS") or self.firstFollow("EQUALS") or self.firstFollow("DIFFERENT") or self.firstFollow("GREATER_EQUAL") or self.firstFollow("LESS_EQUAL"):
-                return True if self.opRelacional() and self.valor() else self.error("relacao")
+                return True if self.opRelacional(node) and self.valor(node) else self.error(node)
             return True
-        return self.error("relacao")
-        
-    def valor(self):
+        return self.error(node)
+
+    def valor(self, parent_node):
         # valor -> expressao | lista | STRING | BOOL
-        print("-> valor")
+        node = self.addNode("valor", parent_node)
         if self.firstFollow("STRING") or self.firstFollow("BOOL"):
-            return True if self.matchTipo("STRING") or self.matchTipo("BOOL") else self.error("valor")
+            return True if self.matchTipo("STRING", node) or self.matchTipo("BOOL", node) else self.error(node)
         if self.firstFollow("OPEN_BRACKET"):
-            return True if self.lista() else self.error("valor")
-        return True if self.expressao() else self.error("valor")
-        
-    def opRelacional(self):
+            return True if self.lista(node) else self.error(node)
+        return True if self.expressao(node) else self.error(node)
+
+    def opRelacional(self, parent_node):
         # opRelacional -> GREATER | LESS | EQUALS | DIFFERENT | GREATER_EQUAL | LESS_EQUAL
-        print("-> opRelacional")
-        return True if self.matchTipo("GREATER") or self.matchTipo("LESS") or self.matchTipo("EQUALS") or self.matchTipo("DIFFERENT") or self.matchTipo("GREATER_EQUAL") or self.matchTipo("LESS_EQUAL") else self.error("opRelacional")
-    
-    def opLogico(self):
+        node = self.addNode("opRelacional", parent_node)
+        return True if self.matchTipo("GREATER", node) or self.matchTipo("LESS", node) or self.matchTipo("EQUALS", node) or self.matchTipo("DIFFERENT", node) or self.matchTipo("GREATER_EQUAL", node) or self.matchTipo("LESS_EQUAL", node) else self.error(node)
+
+    def opLogico(self, parent_node):
         # opLogico -> AND | OR
-        print("-> opLogico")
-        return True if self.matchTipo("AND") or self.matchTipo("OR") else self.error("opLogico")
-    
-    def expressao(self):
+        node = self.addNode("opLogico", parent_node)
+        return True if self.matchTipo("AND", node) or self.matchTipo("OR", node) else self.error(node)
+
+    def expressao(self, parent_node):
         # expressao -> termo opAd termo | termo
-        print("-> expressao")
-        if self.termo():
+        node = self.addNode("expressao", parent_node)
+        if self.termo(node):
             if self.firstFollow("PLUS") or self.firstFollow("MINUS"):
-                return True if self.opAd() and self.termo() else self.error("expressao")
+                return True if self.opAd(node) and self.termo(node) else self.error(node)
             return True
-        
-    def opAd(self):
+
+    def opAd(self, parent_node):
         # opAd -> PLUS | MINUS
-        print("-> opAd")
-        return True if self.matchTipo("PLUS") or self.matchTipo("MINUS") else self.error("opAd")
-    
-    def termo(self):
+        node = self.addNode("opAd", parent_node)
+        return True if self.matchTipo("PLUS", node) or self.matchTipo("MINUS", node) else self.error(node)
+
+    def termo(self, parent_node):
         # termo -> fator opMul fator | fator
-        print("-> termo")
-        if self.fator():
+        node = self.addNode("termo", parent_node)
+        if self.fator(node):
             if self.firstFollow("MULT") or self.firstFollow("DIV"):
-                return True if self.opMul() and self.fator() else self.error("termo")
+                return True if self.opMul(node) and self.fator(node) else self.error(node)
             return True
-        
-    def opMul(self):
+
+    def opMul(self, parent_node):
         # opMul -> MULT | DIV
-        print("-> opMul")
-        return True if self.matchTipo("MULT") or self.matchTipo("DIV") else self.error("opMul")
-    
-    def fator(self):
+        node = self.addNode("opMul", parent_node)
+        return True if self.matchTipo("MULT", node) or self.matchTipo("DIV", node) else self.error(node)
+
+    def fator(self, parent_node):
         # fator -> NUM | DOUBLE | ID | OPEN_PARENTHESIS expressao CLOSE_PARENTHESIS
-        print("-> fator")
-        if self.matchTipo("NUMBER") or self.matchTipo("DOUBLE") or self.matchTipo("ID"):
+        node = self.addNode("fator", parent_node)
+        if self.matchTipo("NUMBER", node) or self.matchTipo("DOUBLE", node) or self.matchTipo("ID", node):
             return True
-        elif self.matchTipo("OPEN_PARENTHESIS"):
-            return True if self.expressao() and self.matchTipo("CLOSE_PARENTHESIS") else self.error("fator")
+        elif self.matchTipo("OPEN_PARENTHESIS", node):
+            return True if self.expressao(node) and self.matchTipo("CLOSE_PARENTHESIS", node) else self.error(node)
         
-        return self.error("fator")
-    
-    def lista(self):
+        return self.error(node)
+
+    def lista(self, parent_node):
         # lista -> OPEN_BRACKET corpoLista CLOSE_BRACKET
-        print("-> lista")
-        return True if self.matchTipo("OPEN_BRACKET") and self.corpoLista() and self.matchTipo("CLOSE_BRACKET") else self.error("lista")
-    
-    def corpoLista(self):
+        node = self.addNode("lista", parent_node)
+        return True if self.matchTipo("OPEN_BRACKET", node) and self.corpoLista(node) and self.matchTipo("CLOSE_BRACKET", node) else self.error(node)
+
+    def corpoLista(self, parent_node):
         # corpoLista -> valor entradaLista | ε
-        print("-> corpoLista")
-        if self.valor():
-            return True if self.entradaLista() else self.error("corpoLista")
-        return True
-    
-    def entradaLista(self):
-        # entradaLista -> COMMA valor entradaLista | ε
-        print("-> entradaLista")
-        if self.matchTipo("COMMA"):
-            return True if self.valor() and self.entradaLista() else self.error("entradaLista")
+        node = self.addNode("corpoLista", parent_node)
+        if self.valor(node):
+            return True if self.entradaLista(node) else self.error(node)
         return True
 
+    def entradaLista(self, parent_node):
+        # entradaLista -> COMMA valor entradaLista | ε
+        node = self.addNode("entradaLista", parent_node)
+        if self.matchTipo("COMMA", node):
+            return True if self.valor(node) and self.entradaLista(node) else self.error(node)
+        return True
+    
 # ========================================
 # >>>>>>>>>>> Classe Parser <<<<<<<<<<<<<
 # ========================================
@@ -155,6 +152,7 @@ class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.token = tokens.pop(0)
+        self.tree = Tree()
         self.rules = Rules(self)
 
     def nextToken(self):
@@ -163,15 +161,18 @@ class Parser:
         else:
             print("Fim do arquivo.")
 
-    def matchLexema(self, lexema):
-        if self.token.lexema == lexema:
+    def matchTipo(self, tipo, parent_node):
+        if self.token.tipo == tipo:
+            terminal_node = self.tree.create_node(f"<{tipo}, {self.token.lexema}>", parent_node)
+            parent_node.add_child(terminal_node)
             self.nextToken()
             return True
         return False
 
-    def matchTipo(self, tipo):
-        if self.token.tipo == tipo:
-            print(f"-- match --> {self.token}")
+    def matchLexema(self, lexema, parent_node):
+        if self.token.lexema == lexema:
+            terminal_node = self.tree.create_node(f"<{self.token.tipo}, {lexema}>", parent_node)
+            parent_node.add_child(terminal_node)
             self.nextToken()
             return True
         return False
@@ -182,12 +183,26 @@ class Parser:
     def eof(self):
         return self.token.tipo == "EOF"
 
-    def error(self, rule):
-        print(f"!-> Error in rule {rule} at line {self.token.linha}")
+    def error(self, node):
+        terminal_node = self.tree.create_node(f"X Erro!", node)
+        node.add_child(terminal_node)
+        print(f"!-> Error in rule {node.name} at line {self.token.linha}")
+        return False
 
     def endBlock(self):
         return self.token.tipo == "DEDENT"
+    
+    def addNode(self, name, parent_node):
+        node = self.tree.create_node(name, parent_node)
+        parent_node.add_child(node)
+        return node
+    
+    def setRoot(self):
+        node = self.tree.create_node("prog", None)
+        self.tree.root = node
+        return node
 
     def parse(self):
-        return self.rules.prog()
-    
+        result = self.rules.prog()
+        self.tree.print_tree()
+        return result
